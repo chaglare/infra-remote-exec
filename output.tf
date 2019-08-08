d570d9ae639ad0eca0af45ec6402edfc88d0aa93
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 19:50:22 -0500

    domain output added

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "pub_sec_group" {
-  value = "${aws_security_group.jenkins.name}"
-}
-
 output "key_name" {
   value = "${aws_key_pair.developer_key.id}"
+}
+
+output "Domain" {
+    value = "${var.record}.${var.domain}"
 }
