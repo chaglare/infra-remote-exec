39e064f9446110621347796497c93ebb4a64cbe9
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 21:43:32 -0500

    edited

--- jenkins/sec_group.tf
+++ jenkins/sec_group.tf
@@ -1,14 +1,7 @@
 resource "aws_security_group" "jenkins" {
-  name        = "public"
+  name        = "jenkins"
   description = "Allow inbound traffic"
   vpc_id      = "${var.vpc}"
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 22
@@ -18,8 +11,8 @@ resource "aws_security_group" "jenkins" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -30,7 +23,6 @@ resource "aws_security_group" "jenkins" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
-
 
   tags = {
       Name = "${var.Name}.public"
