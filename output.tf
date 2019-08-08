4610eab6debb3bf5278dfd3366dd49cc1a6f518b
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 19:51:44 -0500

    edited

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "key_name" {
   value = "${aws_key_pair.developer_key.id}"
 }
 
-output "Domain" {
-    value = "${var.record}.${var.domain}"
+output "Link" {
+    value = "${var.record}.${var.domain}:8080"
 }
