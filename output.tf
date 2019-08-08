52cf0d64719cd57aa402a53f6c1aca2261479d0f
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 19:45:49 -0500

    edited

--- output.tf
+++ output.tf
@@ -1,11 +1,7 @@
-output "VPC" {
-  value = "${aws_vpc.dev.id}"
-}
-
 output "pub_sec_group" {
   value = "${aws_security_group.jenkins.name}"
 }
 
 output "key_name" {
-  value = "${var.key_name}"
+  value = "${aws_key_pair.developer_key.id}"
 }
