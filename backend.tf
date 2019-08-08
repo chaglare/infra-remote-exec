ebe8f686582f177fbd39238c45e439d49ed86566
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 19:14:22 -0500

    edited

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        region = us-east-1"
-        bucker = "remote-exec-caglar"
-        key = 
+        region  =  "us-east-1"
+        bucket = "remote-exec-caglar"
+        key = "remote.state"
     }
 }
